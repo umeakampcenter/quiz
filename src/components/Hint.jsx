4d72c921d61b3299912c74417e46340ca3b8ad2b
Peter Spegel <peter.spegel.umea@gmail.com>
2017-09-03 21:07:42 +0200

    Lägger till funktionalitet för att visa upp ord med placeholders

--- src/components/Hint.jsx
+++ src/components/Hint.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export default class Hint extends React.Component {
+  wrap(text) {
+    return text.split('').map(t => {
+      if (t === ' ') {
+        return <div className="space"></div>;
+      }
+      return <div className="letter">{t}</div>;
+    });
+  }
+
   render() {
     return (
       <div className="hint">
-        {this.props.hintsUsed}
+        {this.wrap(this.props.answer)}
       </div>
     );
   }
