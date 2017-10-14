dbf57e60f18c44775b20ae16f63aee4850fbeaa6
Peter Spegel <peter.spegel@acino.se>
2017-10-14 21:42:54 +0200

    Lägger till så att även varje ord i ledtråden wrappas i en div.

--- src/components/Hint.jsx
+++ src/components/Hint.jsx
@@ -3,14 +3,18 @@ import update from 'immutability-helper';
 
 export default class Hint extends React.Component {
   wrap() {
-    return this.props.answer.split('').map((char, i) => {
-      if (char === ' ') {
-        return <div className="space" key={i}></div>;
-      }
-      if (!this.props.answerFilter[i]) {
-        return <div className="letter" key={i}>&nbsp;</div>;
-      }
-      return <div className="letter" key={i}>{char}</div>;
+    const words = this.props.answer.split(' ');
+    return words.map((word, i) => {
+      const wrappedLetters = word.split('').map((char, j) => {
+        if (char === ' ') {
+          return <div className="space" key={j}></div>;
+        }
+        if (!this.props.answerFilter[j]) {
+          return <div className="letter" key={j}>&nbsp;</div>;
+        }
+        return <div className="letter" key={j}>{char}</div>;
+      });
+      return <div className="word" key={i}>{wrappedLetters}</div>;
     });
   }
 
