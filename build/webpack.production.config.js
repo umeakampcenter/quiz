f4c58f3aad1644cee6e50b999b23947f494332a1
Peter Spegel <peter.spegel@acino.se>
2017-10-14 19:28:39 +0200

    Ändringar för att kunna prodsätta.

--- build/webpack.production.config.js
+++ build/webpack.production.config.js
@@ -41,6 +41,26 @@ module.exports = {
           use: 'css-loader!sass-loader',
         }),
       },
+      // the url-loader uses DataUrls. 
+      // the file-loader emits files. 
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          { loader: 'file-loader' }
+        ]
+      }
     ],
   },
 
